b6074ebff04cd04d0235c2ba90516a8956758d5c
Darko Azinović <darko.azinovic@gmail.com>
2026-09-08 20:54:49 +0200

    jQuery

---
+++ 03_JS/vjezba20/rucno/app.js
@@ -0,0 +1,12 @@
+$('#pozdravi').click(function(){
+    $('#pozdrav').text('Hello world')
+    $('#sakrij').show()
+})
+
+
+$('#sakrij').hide()
+
+$('#sakrij').click(function(){
+    $('#pozdrav').text('')
+    $('#sakrij').hide()
+})
