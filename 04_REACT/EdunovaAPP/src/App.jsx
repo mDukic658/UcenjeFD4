60bcb289c98d89e1c17369cd96a73a31ec06b6ec
Marta Đukić <martadukic2004@gmail.com>
2026-09-17 20:50:03 +0200

    upogonili react bootstrap

--- 04_REACT/EdunovaAPP/src/App.jsx
+++ 04_REACT/EdunovaAPP/src/App.jsx
@@ -1,10 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Badge, Container } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-      hello
+      <Container>
+        Hello 
+      </Container>
+      <hr />
+      <div className='podrucje'>
+        Edunova
+      </div>
+
+      <Badge bg='success'>5</Badge>
+
     </>
   )
 }
