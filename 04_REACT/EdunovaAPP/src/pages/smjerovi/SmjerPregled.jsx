3b6f58f079fd1ee73ff4426b5e6785db904d2d34
Marta Đukić <martadukic2004@gmail.com>
2026-09-24 20:10:33 +0200

    Prikaz podataka smjera u formi tablice

--- 04_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 04_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,30 +1,80 @@
 import { useEffect, useState } from "react"
 import SmjerService from "../../services/smjerovi/SmjerService"
+import { Badge, Table } from "react-bootstrap"
+import { GrValidate } from "react-icons/gr"
+import { FcApproval, FcDisapprove } from "react-icons/fc"
 
 
-export default function SmjerPregled(){
+export default function SmjerPregled() {
 
     const [smjerovi, setSmjerovi] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('Došao na pregled smjerova')
         ucitajSmjerove()
-    },[])
+    }, [])
 
-    async function ucitajSmjerove(){
-        await SmjerService.get().then((odgovor)=>{
-           // console.table(odgovor.data)
-           setSmjerovi(odgovor.data)
+    async function ucitajSmjerove() {
+        await SmjerService.get().then((odgovor) => {
+            // console.table(odgovor.data)
+            setSmjerovi(odgovor.data)
         })
     }
 
-    return(
+    return (
         <>
-            Ovdje dođe pregled smjerova
-            <hr />
-            <pre>
+
+            <Table hover striped bordered>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Trajanje</th>
+                        <th>Cijena</th>
+                        <th>Datum pokretanja</th>
+                        <th>Aktivan</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {smjerovi && smjerovi.map((smjer) => (
+
+                        <tr key={smjer.sifra}>
+                            <td> {smjer.naziv} </td>
+                            <td> {smjer.trajanje} </td>
+                            <td> {smjer.cijena} </td>
+                            <td> {smjer.datumPokretanja} </td>
+                            {/* <td> {smjer.aktivan ? 'DA' : 'NE'} </td> */}
+
+                            <td>
+                                <GrValidate
+                                    size={25}
+                                    color={smjer.aktivan ? 'green' : 'red'}
+                                    title={smjer.aktivan ? 'Aktivan' : 'Neaktivan'}
+                                />
+
+                                {smjer.aktivan ? (
+                                    <FcApproval />
+                                ) : (
+                                    <FcDisapprove />
+                                )}
+
+                            </td>
+
+                        </tr>
+
+                    ))}
+                </tbody>
+            </Table>
+
+            Ukupno &nbsp;
+            <Badge pill bg="success">
+                {smjerovi && smjerovi.length} 
+            </Badge>
+            &nbsp; smjerova
+
+            {/* <pre>
                 {JSON.stringify(smjerovi, null, 2)}
-            </pre> 
+            </pre> */}
         </>
     )
 }
